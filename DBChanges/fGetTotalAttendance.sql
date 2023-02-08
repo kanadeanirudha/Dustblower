93fc9760405df64fe7711941121c601be708b63a
anirudha-kanade-amla <anirudha.kanade@amla.io>
2023-02-08 17:53:22 +0530

    Changes for duplicate Attendance

--- DBChanges/fGetTotalAttendance.sql
+++ DBChanges/fGetTotalAttendance.sql
@@ -1,5 +1,3 @@
-USE [Ver1]
-GO
 /****** Object:  UserDefinedFunction [dbo].[fGetTotalAttendance]    Script Date: 2/7/2023 9:13:04 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -10,15 +8,15 @@ GO
 (@EmployeeId int,@iSaleContractBillingSpanID bigint,@EndDate date) returns nvarchar(100) 
 AS
 BEGIN
-Declare @TotalAttendance nVarchar(100);
+Declare @TotalAttendance nVarchar(MAX);
 ;
 with cteSaleContractBillingSpan as 
 (
 select 
-	A.SaleContractMasterID, A.ID as SaleContractBillingSpanId , month(EndDate) as SalMonth, YEAR(EndDate) as SalYear 
+	A.SaleContractMasterID, A.ID as SaleContractBillingSpanId , month(EndDate) as SalMonth, YEAR(EndDate) as SalYear, B.ContractNumber 
 from 
 	SaleContractBillingSpan A
-where  cast (datepart(YEAR, EndDate) as  varchar(5)) +  cast(datepart(Month, EndDate)as varchar(2) ) =cast (datepart(YEAR, @EndDate) as  varchar(5)) +  cast(datepart(Month, @EndDate)as varchar(2) )
+	INNER JOIN SaleContractMaster B ON A.SaleContractMasterID=B.ID AND  cast (datepart(YEAR, EndDate) as  varchar(5)) +  cast(datepart(Month, EndDate)as varchar(2) ) =cast (datepart(YEAR, @EndDate) as  varchar(5)) +  cast(datepart(Month, @EndDate)as varchar(2) )
       and A.ID != @iSaleContractBillingSpanID
 --and isbillgenerated=1
 ), cteFianl as 
@@ -27,15 +25,30 @@ select
 	A.SaleContractEmployeeMasterID, 
 	B.SalMonth, 
 	B.SalYear,
-	isnull(sum(A.TotalAttendance),0) as TotalAttendance
+	A.TotalAttendance
+	,B.ContractNumber
+	--isnull(sum(A.TotalAttendance),0) as TotalAttendance
 from 
 	SaleContractAttendance A 
 inner join cteSaleContractBillingSpan B on (A.SaleContractBillingSpanID=b.SaleContractBillingSpanId and A.SaleContractEmployeeMasterID=@EmployeeId and A.ApprovalStatus=2) 
+--group by A.SaleContractEmployeeMasterID, SalYear,SalMonth
+),cteFinal2 as (
+select
+	A.SaleContractEmployeeMasterID, 
+	A.SalMonth, 
+	A.SalYear,
+	isnull(sum(A.TotalAttendance),0) as TotalAttendance
+	,STUFF(( SELECT ',' + CAST(innerTable.ContractNumber AS nvarchar(MAX))
+	FROM cteFianl AS innerTable
+	WHERE innerTable.SaleContractEmployeeMasterID= a.SaleContractEmployeeMasterID
+	FOR XML PATH('')
+	),1,1,'') AS ContractNumbers
+from cteFianl A
 group by A.SaleContractEmployeeMasterID, SalYear,SalMonth
 )
-select @TotalAttendance= cast( TotalAttendance as  varchar(100)) +'~'
+select @TotalAttendance= cast( TotalAttendance as  varchar(100)) +'~'+ContractNumbers
  from
-cteFianl
+cteFinal2
 
 return isnull(@TotalAttendance,'0~')
 end
